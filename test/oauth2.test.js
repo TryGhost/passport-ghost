22f713157e4041213411fd255c0928846af9e41e
kirrg001 <katharina.irrgang@googlemail.com>
2016-10-13 11:47:48 +0200

    ✨  more tests

--- test/oauth2.test.js
+++ test/oauth2.test.js
@@ -1,7 +1,12 @@
 var oauth2 = require('../lib/oauth2'),
-    errors = require('ghost-ignition').errors;
+    sinon = require('sinon'),
+    sandbox = sinon.sandbox.create();
 
 describe('Ghost Oauth2', function () {
+    afterEach(function () {
+        sandbox.restore();
+    });
+
     describe('instantiate', function () {
         it('no verify callback', function () {
             try {
@@ -52,6 +57,151 @@ describe('Ghost Oauth2', function () {
         it('no access token', function (done) {
             ghostStrategy.userProfile(null, function (err) {
                 should.exist(err);
+                done();
+            });
+        });
+
+        it('with access token: cant parse body', function (done) {
+            sandbox.stub(ghostStrategy._oauth2, 'get', function (url, token, profileDone) {
+                profileDone(null, 'body');
+            });
+
+            ghostStrategy.userProfile('access-token', function (err) {
+                should.exist(err);
+                done();
+            });
+        });
+
+        it('with access token', function (done) {
+            sandbox.stub(ghostStrategy._oauth2, 'get', function (url, token, profileDone) {
+                token.should.eql('access-token');
+                url.should.eql('http://my-ghost-auth-server/oauth2/userinfo');
+                profileDone(null, JSON.stringify({profile: 'katharina'}));
+            });
+
+            ghostStrategy.userProfile('access-token', function (err, response) {
+                should.not.exist(err);
+                should.exist(response.profile);
+                done();
+            });
+        });
+    });
+
+    describe('register client', function () {
+        var ghostStrategy;
+
+        before(function () {
+            ghostStrategy = new oauth2.Strategy({
+                callbackURL: 'http://localhost:8888/callback',
+                passReqToCallback: true,
+                url: 'http://my-ghost-auth-server'
+            }, function verifyCallback() {
+            });
+        });
+
+        it('error', function () {
+            sandbox.stub(ghostStrategy._oauth2, '_request', function (method, url, headers, body, query, requestDone) {
+                method.should.eql('POST');
+                url.should.eql('http://my-ghost-auth-server/oauth2/client');
+                headers['content-type'].should.eql('application/json');
+                (typeof body).should.eql('string');
+                JSON.parse(body).client_name.should.eql('client');
+                JSON.parse(body).redirect_uri.should.eql('http://localhost:8888/callback');
+
+                requestDone(new Error('connection refused'));
+            });
+
+            return ghostStrategy.registerClient()
+                .catch(function (err) {
+                    should.exist(err);
+                });
+        });
+
+        it('no options, expect default client name', function () {
+            sandbox.stub(ghostStrategy._oauth2, '_request', function (method, url, headers, body, query, requestDone) {
+                method.should.eql('POST');
+                url.should.eql('http://my-ghost-auth-server/oauth2/client');
+                headers['content-type'].should.eql('application/json');
+                (typeof body).should.eql('string');
+                JSON.parse(body).client_name.should.eql('client');
+                JSON.parse(body).redirect_uri.should.eql('http://localhost:8888/callback');
+                requestDone(null, JSON.stringify({client_id: '1'}));
+            });
+
+            return ghostStrategy.registerClient()
+                .then(function (response) {
+                    response.client_id.should.eql('1');
+                });
+        });
+
+        it('with options', function () {
+            sandbox.stub(ghostStrategy._oauth2, '_request', function (method, url, headers, body, query, requestDone) {
+                method.should.eql('POST');
+                url.should.eql('http://my-ghost-auth-server/oauth2/client');
+                headers['content-type'].should.eql('application/json');
+                (typeof body).should.eql('string');
+                JSON.parse(body).client_name.should.eql('my-blog');
+                JSON.parse(body).redirect_uri.should.eql('http://localhost:8888/callback');
+                requestDone(null, JSON.stringify({client_id: '1'}));
+            });
+
+            return ghostStrategy.registerClient({clientName: 'my-blog'})
+                .then(function (response) {
+                    response.client_id.should.eql('1');
+                });
+        });
+    });
+
+    describe('change pwd', function () {
+        var ghostStrategy;
+
+        before(function () {
+            ghostStrategy = new oauth2.Strategy({
+                callbackURL: 'http://localhost:8888/callback',
+                passReqToCallback: true,
+                url: 'http://my-ghost-auth-server'
+            }, function verifyCallback() {
+            });
+        });
+
+        it('error: no data', function (done) {
+            sandbox.stub(ghostStrategy._oauth2, '_request', function (method, url, headers, body, query, requestDone) {
+                method.should.eql('PUT');
+                url.should.eql('http://my-ghost-auth-server/oauth2/password');
+                headers['content-type'].should.eql('application/json');
+                (typeof body).should.eql('string');
+                should.not.exist(JSON.parse(body).access_token);
+                should.not.exist(JSON.parse(body).oldPassword);
+
+                requestDone(new Error('validation error'));
+            });
+
+            ghostStrategy.changePassword(null, function (err) {
+                should.exist(err);
+                done();
+            });
+        });
+
+        it('success', function (done) {
+            sandbox.stub(ghostStrategy._oauth2, '_request', function (method, url, headers, body, query, requestDone) {
+                method.should.eql('PUT');
+                url.should.eql('http://my-ghost-auth-server/oauth2/password');
+                headers['content-type'].should.eql('application/json');
+                (typeof body).should.eql('string');
+                JSON.parse(body).access_token.should.eql('a');
+                JSON.parse(body).oldPassword.should.eql('b');
+                JSON.parse(body).newPassword.should.eql('c');
+
+                requestDone(null, JSON.stringify({something: 'test'}));
+            });
+
+            ghostStrategy.changePassword({
+                accessToken: 'a',
+                oldPassword: 'b',
+                newPassword: 'c'
+            }, function (err, response) {
+                should.not.exist(err);
+                should.exist(response.something);
                 done();
             });
         });
